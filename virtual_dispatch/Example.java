6e623114d87bddb8f391088d9258b354d7b279d4
Kyle Dewey <kyle.dewey@csun.edu>
2021-01-28 13:47:15 -0800

    Some additions

--- virtual_dispatch/Example.java
+++ virtual_dispatch/Example.java
@@ -4,7 +4,25 @@
 //   -If writing to a file, which file?
 //
 public class Example {
+    public static void doComputation() {
+        // does something
+
+        int intermediateResult = ...;
+        
+        // print intermediate result
+        // print it out
+        if (userWantsConsole) {
+            System.out.println(intermediateResult);
+        } else {
+            File file = new File(destinationFile);
+            FileOutputStream stream = new FileOutputStream(file);
+            stream.writeln(intermediateResult);
+            stream.close();
+        }
+    }
+    
     public static void main(String[] args) {
+        // parsing command line arguments
         boolean userWantsConsole = doesUserWantConsole(args);
         String destinationFile = getDestinationFile(args);
 
