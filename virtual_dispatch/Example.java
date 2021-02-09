e1270a3d534a1d33fc3da2845fcedf056c10d93f
Kyle Dewey <kyle.dewey@csun.edu>
2021-02-09 13:49:33 -0800

    added some notes we discussed in class

--- virtual_dispatch/Example.java
+++ virtual_dispatch/Example.java
@@ -14,11 +14,29 @@ public class Example {
         destination.writeThing(intermediateResult);
     }
 
+    // ---begin method overloading---
+    //
+    // virtual dispatch: determination done at runtime
+    // overloading: determination done at compile time
+    // 
+    // doSomething(42) // calls first one
+    // doSomething(false) // call second one
+    //
+    public static void doSomething(int x) {}
+    public static void doSomething(boolean b) {}
+    public static void doSomething(int x, int y) {}
+    // ---end method overloading---
+    
+    // Uses subtyping
+    // - (sub)type polymorphism
+    // - polymorphism
     public static OutputDestination makeDestination(boolean userWantsConsole,
                                                     String destinationFile) {
         if (userWantsConsole) {
+            // ConsoleDestination is a subtype of OutputDestination
             return new ConsoleDestination();
         } else {
+            // FileDestination is a subtype of OutputDestination
             return new FileDestination(destinationFile);
         }
     }
@@ -36,6 +54,10 @@ public class Example {
         // print it out
         // virtual dispatch: runtime knows which method to call,
         // when at compile time it could be different methods
+        // virtual dispatch is the same thing as
+        // - dynamic dispatch
+        // - ad-hoc polymorphism
+        // - polymorphism
         destination.writeThing(result);
 
         // close it
