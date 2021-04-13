dabb677bd7596ed99501421f1086cefc403663ea
Kyle Dewey <kyle.dewey@csun.edu>
2021-04-13 13:46:35 -0700

    added stuff from today on implementation

--- swift/generics.swift
+++ swift/generics.swift
@@ -59,3 +59,52 @@ func tupleStuff(tup: (Int, Int)) -> (String, String) {
 }
 
 tupleStuff(tup: (3, 4))
+
+// let emptyTuple = ()
+
+//func returnFunction() -> ((Int, String) -> String) { ... }
+
+func funcTwo<A,B>(_ a: A, _ b: B) -> (A,B) {
+  return (a,b);
+}
+
+let temp1: (Int, Int) = funcTwo(1, 2)
+let temp2: (Int, String) = funcTwo(1, "foo")
+let temp3: (Int, Int) = funcTwo(3, 4)
+
+// C++-style of handling generics - specialize the generic definition
+// per whatever types you work with
+func funcTwo_int_int(_ a: Int, _ b: Int) -> (Int, Int) {
+  return (a,b);
+}
+
+func funcTwo_int_string(_ a: Int, _ b: String) -> (Int, String) {
+  return (a,b);
+}
+
+let temp1: (Int, Int) = funcTwo_int_int(1, 2)
+let temp2: (Int, String) = funcTwo_int_string(1, "foo")
+let temp3: (Int, Int) = funcTwo_int_int(3, 4)
+
+
+// Java-style of handling generics - boxing
+
+// public static <A, B> Pair<A, B> funcTwo(A a, B b) {
+//   return new Pair(a, b);
+// }
+//
+// Pair<Int, Int> temp1 = funcTwo(1, 2);
+// Int temp1Int = temp1.firstValue;
+//
+//
+// public static Pair funcTwo(Object a, Object b) {
+//   return new Pair(a, b);
+// }
+//
+// Pair temp1 = funcTwo(1, 2);
+// Int temp1Int = (Int)temp1.firstValue;
+
+
+// Rust - gives you control
+
+// Go - no generics / parametric polymorphism
