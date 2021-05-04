59afbdb22cb56533d8474cf1875c8d1f3187e684
Kyle Dewey <kyle.dewey@csun.edu>
2021-05-04 13:15:03 -0700

    .

--- prolog/intro.pl
+++ prolog/intro.pl
@@ -15,5 +15,47 @@ isInteger(0). % fact
 % All the facts (and rules) you give it describe all truth.
 % Anything not in those facts in rules is false.
 
-% fact: any X is equal to itself
-areEqual(X, X).
+% areEqual/2 procedure
+areEqual(X, X). % fact: any X is equal to itself
+
+% isName/1 procedure
+isName(kyle).   % fact
+isName(janet).  % fact
+isName(bob).    % fact
+isName(alice).  % fact
+
+isWarm(pizza).
+isWarm(burgers).
+isWarm(burritos).
+
+
+likes(alice,pizza).
+likes(alice,burgers).
+likes(alice,burritos).
+likes(alice,yogurt).
+likes(alice,pizza).
+likes(bob,pizza).
+likes(bob,burgers).
+likes(bob,salad).
+likes(bob,milk).
+
+% inclusive range
+%% between5And10(5).
+%% between5And10(6).
+%% between5And10(7).
+%% between5And10(8).
+%% between5And10(9).
+%% between5And10(10).
+
+% between5And10/1 procedure
+between5And10(Number) :-
+    inRange(Number, 5, 10).
+
+% inRange(Number, Min, Max) <= (Min <= Number && Number =< Max).  % q <= p
+% (Min <= Number && Number =< Max) => inRange(Number, Min, Max).  % p => q
+
+% inRange/3 procedure
+%% inRange(0, 0, 0).
+inRange(Number, Min, Max) :-
+    Min =< Number, % one clause
+    Number =< Max. % checks if Number is <= Max
